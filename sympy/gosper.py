a31d89fe9641103a473e9e05d8a9f473bfb709e6
Calvin Higgins <calvin_higgins2@uri.edu>
2023-03-20 12:59:41 -0400

    IT WORKS!

--- sympy/gosper.py
+++ sympy/gosper.py
@@ -1,5 +1,5 @@
 from sympy import fraction, combsimp, cancel, resultant, roots, Symbol, Dummy
-from sympy import gcd, degree
+from sympy import gcd, degree, linsolve, simplify
 
 
 def compute_bc(a, k):
@@ -30,6 +30,9 @@ def compute_pqr(b, c, k):
             p *= g.subs(k, k + i)
 
     # TODO: postconditions
+    p = cancel(p)
+    q = cancel(q)
+    r = cancel(r)
     return p, q, r
 
 
@@ -38,24 +41,76 @@ def degree_bound_f(p, q, r, k):
     y = q - r.subs(k, k-1)
 
     n = degree(x, k)
-    print(x, y)
     if n <= degree(y, k):
         return degree(p.subs(k, k-1), k) - degree(y, k)
     
     a = x.coeff(k, n)
     b = y.coeff(k, n - 1)
     assert not a.equals(0)
-    print(a, b, n)
 
     z = (-2 * b) / a
     if not z.is_integer or z < 0:
         return degree(p.subs(k, k-1), k) - n + 1
     
-    print(z, degree(p.subs(k, k-1), k) - n + 1)
     return max(z, degree(p.subs(k, k-1), k) - n + 1)
+
+
+def compute_f(p, q, r, d, k):
+    f = 0
+    cs = []
+    for i in range(0, d + 1):
+        c = Dummy(f"c{i}")
+        f += c * (k ** i)
+        cs.append(c)
+    
+    print(f)
+    p.subs(k, k-1)
+    e = q * f - r.subs(k, k-1) * f.subs(k, k-1) - p.subs(k, k-1)
+    print("e: ", e)
+    print("e: ", simplify(e))
+    es = []
+    for i in range(0, 2 * (d + 1)): # TODO: idk why we need * 2
+        es.append(e.subs(k, i))
+    
+    print(es)
+    ss = linsolve(es, cs)
+    if len(ss) != 1:
+        raise ValueError()
+
+    s = next(iter(ss))
+    f = f.subs(list(zip(cs, s)))
+    f = f.subs(list(zip(cs, [0] * len(cs))))
+    # print(f)
+    return f
+
+
+def compute_s(a, p, r, f, k):
+    s = (r.subs(k, k-1) / p.subs(k, k-1)) * f.subs(k, k-1) * a
+    s = cancel(s)
+    return s
+
+k = Symbol('k', integer=True)
+# p = k
+# q = k ** 2 - k
+# r = k ** 2
+# d = degree_bound_f(p, q, r, k)
+# print(d)
+# compute_f(p, q, r, d, k)
 
 
 def gosper_sum(a, k):
     b, c = compute_bc(a, k)
     p, q, r = compute_pqr(b, c, k)
-    d = degree_bound_f(q, p, r, k)
+    print(p, q, r)
+    d = degree_bound_f(p, q, r, k)
+    print(d)
+    if d < 0:
+        raise ValueError()
+    f = compute_f(p, q, r, d, k)
+    s = compute_s(a, p, r, f, k)
+    return s
+
+from sympy import Integer
+k = Symbol('k', integer=True)
+a = (2 ** k) * (k ** 8) * (3 ** k)
+print(gosper_sum(a, k))
